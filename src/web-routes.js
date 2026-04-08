3710c413019baac938b7b34e748d44ad74761d15
Andre <andre.luismp@gmail.com>
2026-04-08 08:40:27 +0200

    donation-hapi-01-app-shell

---
+++ src/web-routes.js
@@ -0,0 +1,29 @@
+import { accountsController } from "./controllers/accounts-controller.js";
+import { donationsController } from "./controllers/donations-controller.js";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export const webRoutes = [
+  { method: "GET", path: "/", config: accountsController.index },
+  { method: "GET", path: "/signup", config: accountsController.showSignup },
+  { method: "GET", path: "/login", config: accountsController.showLogin },
+  { method: "GET", path: "/logout", config: accountsController.logout },
+  { method: "POST", path: "/register", config: accountsController.signup },
+  { method: "POST", path: "/authenticate", config: accountsController.login },
+
+  { method: "GET", path: "/donate", config: donationsController.index },
+
+  {
+    method: "GET",
+    path: "/{param*}",
+    handler: {
+      directory: {
+        path: path.join(__dirname, "public"),
+      },
+    },
+    options: { auth: false },
+  },
+];
